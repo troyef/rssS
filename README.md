eeaf66e6b00557825e89521b12ce2f509d793f50
troyef <troyef@gmail.com>
2014-05-11 11:39:42 -0500

    initial commit

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ rssS
 ====
 
 A simple rss to json Service.
+
+This app just utilizes a couple other libraries to create a service that will pull an RSS feed and convert it to json for use in a web app.
---
+++ server.js
@@ -0,0 +1,62 @@
+var express = require('express'),
+  http = require('http'),
+  feedparser = require('ortoo-feedparser'),
+  app = express();
+
+
+
+
+app.set('port', 3001);
+
+app.get('/feed/:url', function(req, res){
+  function callback (meta, articles) {
+    res.type('application/json');
+    res.jsonp({ meta: meta, articles: articles});
+  }
+
+  console.log('Getting ' + req.params.url);
+  feedparser.parseUrl(req.params.url)
+    .on('complete', callback);
+  //res.send(req.params[0]);
+});
+
+app.get('/feeds', function(req, res){
+  console.log('In the feeds ');
+  console.log(req.query.urls);
+  var urls = req.query.urls || [],
+    results = [],
+    i = 0,
+    max = urls.length;
+
+  if (max > 0){
+    for (;i < max;i++){
+      console.log('Getting ' + urls[i]);
+      feedparser.parseUrl(urls[i])
+        .on('complete', callback);
+
+    }
+  } else {
+    sendResults();
+  }
+
+function callback (meta, articles) {
+  results.push({ meta: meta, articles: articles});
+
+  if (results.length === max){
+        sendResults();
+  }
+}
+function sendResults(){
+  res.type('application/json');
+  res.jsonp(results);
+}
+
+
+});
+
+
+
+
+http.createServer(app).listen(app.get('port'), function(){
+    console.log('Express server listening on port ' + app.get('port'));
+});
